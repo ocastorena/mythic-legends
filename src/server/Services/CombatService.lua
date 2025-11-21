72794b066b8fdd67116f2dcc9c5526948d465686
ocastorena <omarcastorena11@gmail.com>
2025-11-21 08:56:59 -0700

    Refactor CombatService

--- src/server/Services/CombatService.lua
+++ src/server/Services/CombatService.lua
@@ -1,52 +1,58 @@
 -- ServerScriptService/Services/CombatService
 
 local Players = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local weaponsData = require(ReplicatedStorage:WaitForChild("Config").Weapons)
+local WeaponsData = nil
 
-local Context = nil
-
-local combatEvent = nil
+local CombatEvent = nil
 
 local CombatService = {}
 
 local function handleCombatEvent(player: Player, action: string?, payload: any?)
-	if action ~= "HitRequest" then return end
-	
+	if action ~= "HitRequest" then
+		return
+	end
+
 	local targetId = payload.targetId
 	local weaponName = payload.weaponName
-	
+
 	if targetId then
 		local targetPlayer = Players:GetPlayerByUserId(targetId)
-		local tHRP = targetPlayer.Character:FindFirstChild("HumanoidRootPart") :: BasePart
-		local aHRP = player.Character:FindFirstChild("HumanoidRootPart") :: BasePart
-		local tgtChar = targetPlayer.Character
-		local tgtHRP = tgtChar and tgtChar:FindFirstChild("HumanoidRootPart")
-		local atkHRP = player.Character and player.Character:FindFirstChild("HumanoidRootPart")
-		if not (tgtHRP and atkHRP) then return end
+		-- local tHRP = targetPlayer.Character:FindFirstChild("HumanoidRootPart") :: BasePart
+		local aChar = player.Character
+		if aChar then
+			local aHRP = aChar:FindFirstChild("HumanoidRootPart") :: BasePart
+			local tgtChar = targetPlayer.Character
+			local tgtHRP = tgtChar and tgtChar:FindFirstChild("HumanoidRootPart")
+			local atkHRP = player.Character and player.Character:FindFirstChild("HumanoidRootPart")
+			if not (tgtHRP and atkHRP) then
+				return
+			end
 
-		-- Ensure target sim-owns their HRP (usually true for characters)
-		pcall(function() tgtHRP:SetNetworkOwner(targetPlayer) end)
-		
-		local dir = (tgtHRP.Position - aHRP.Position).Unit
-		--local deltaV = dir * (push.horizSpeed or 80) + Vector3.yAxis * (push.upSpeed or 36)
-		local deltaV = dir * weaponsData[weaponName].HoriForce + Vector3.yAxis * weaponsData[weaponName].VertForce
-		-- Tell the target client to impulse (mass*Δv) + ragdoll
-		Context.Remotes.CombatEvent:FireClient(targetPlayer, "HitResponse", {
-			deltaV = deltaV,
-			hitType = weaponsData[weaponName].HitType,
-			hitDuration = weaponsData[weaponName].HitDuration,
-		}) 
+			-- Ensure target sim-owns their HRP (usually true for characters)
+			pcall(function()
+				tgtHRP:SetNetworkOwner(targetPlayer)
+			end)
+
+			local dir = (tgtHRP.Position - aHRP.Position).Unit
+			--local deltaV = dir * (push.horizSpeed or 80) + Vector3.yAxis * (push.upSpeed or 36)
+			local deltaV = dir * WeaponsData[weaponName].HoriForce + Vector3.yAxis * WeaponsData[weaponName].VertForce
+			-- Tell the target client to impulse (mass*Δv) + ragdoll
+			CombatEvent:FireClient(targetPlayer, "HitResponse", {
+				deltaV = deltaV,
+				hitType = WeaponsData[weaponName].HitType,
+				hitDuration = WeaponsData[weaponName].HitDuration,
+			})
+		end
 	end
 end
 
 function CombatService.Init(context: any)
-	Context = context
-	combatEvent = Context.Remotes.CombatEvent
-	
-	combatEvent.OnServerEvent:Connect(handleCombatEvent)
-	
+	WeaponsData = context.Metadata.WeaponsData
+	CombatEvent = context.Remotes.CombatEvent
+
+	CombatEvent.OnServerEvent:Connect(handleCombatEvent)
+
 	print("[CombatService] Initialized")
 end
 
